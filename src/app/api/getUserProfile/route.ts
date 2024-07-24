ad26bba5bee7f340d526e8f27fb84b1978116f83
adw-ith <adwaithraj45654@gmail.com>
2024-07-24 23:47:26 +0530

    client side complete

--- src/app/api/getUserProfile/route.ts
+++ src/app/api/getUserProfile/route.ts
@@ -4,19 +4,20 @@ import clientPromise from "@/lib/mongodb";
 export async function GET(req: any, res: any) {
   //@ts-ignore
   const client = await clientPromise;
-  const db =await client.db("data");
-  const membership_id=req.nextUrl.searchParams.get("membership_id");
-  if(membership_id)
-  {
-const user =await db.collection("users").findOne({membership_id:membership_id},
-  { projection: { password: 0 } });
-const [password,...rest]=user
-  if(user)
-  {
-    return new Response(
+  const db = await client.db("data");
+  const membership_id = req.nextUrl.searchParams.get("membership_id");
+  if (membership_id) {
+    const user = await db
+      .collection("users")
+      .findOne(
+        { membership_id: membership_id },
+        { projection: { password: 0 } }
+      );
+    if (user) {
+      return new Response(
         JSON.stringify({
           message: "user found",
-          user:rest
+          user: user,
         }),
         {
           status: 200,
@@ -25,13 +26,11 @@ const [password,...rest]=user
           },
         }
       );
-  }
-  else
-  {
-    return new Response(
+    } else {
+      return new Response(
         JSON.stringify({
           message: "user not found",
-          user:user
+          user: user,
         }),
         {
           status: 303,
@@ -40,21 +39,23 @@ const [password,...rest]=user
           },
         }
       );
-  }
-}
-else{
-    const users=await db.collection("users").find({},{ projection: { password: 0 } }).toArray()
+    }
+  } else {
+    const users = await db
+      .collection("users")
+      .find({}, { projection: { password: 0 } })
+      .toArray();
     return new Response(
-        JSON.stringify({
-          message: "all users",
-          users:users
-        }),
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-}
+      JSON.stringify({
+        message: "all users",
+        users: users,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -2,69 +2,115 @@
 import clientPromise from "@/lib/mongodb";
 import { Request } from "@/types/types";
 
-export async function POST(req:any,res:any)
-{
-   //@ts-ignore
-    const client = await clientPromise;
-    const db =await client.db('data'); 
-    const {membership_id,component_name,number}=await req.json();
-    const component=await db.collection('components').findOne({ name:component_name})
-    if(component)
-    {
-        if(component.number<number)
+export async function POST(req: any, res: any) {
+  //@ts-ignore
+  const client = await clientPromise;
+  const db = await client.db("data");
+  const { membership_id, component_name, number } = await req.json();
+  const component = await db
+    .collection("components")
+    .findOne({ name: component_name });
+  if (component) {
+    if (component.number < number) {
+      return new Response(
+        JSON.stringify({
+          message: "item's available number is less than the request",
+        }),
         {
-            return new Response(
-                JSON.stringify({
-                  message: "item's available number is less than the request",
-                }),
-                {
-                  status: 300,
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                }
-              ); 
+          status: 300,
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        else{
-            const newRequest:Request={
-              component_name:component_name,
-              membership_id:membership_id,
-              number:number
-            }
-          const newRequestres=await db.collection('requests').insertOne(newRequest);
-          const requested_component={
-            name:component_name,
-            number:number
-          }
-          const updateUserRes=await db.collection('users').updateOne(
-            {  membership_id:membership_id },
-            { $push: {requested_components:requested_component } }
-        )
-        return new Response(
-          JSON.stringify({
-            message: "request made successfully",
-          }),
-          {
-            status: 200,
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        ); 
-        }
-      
-    }
-    else{
-        return new Response(
+      );
+    } else {
+      const existing = await db
+        .collection("requests")
+        .findOne({ component_name, membership_id });
+
+      if (existing) {
+        if (existing.number + number <= component.number) {
+          existing.number += number;
+          const updateRequestRes = await db
+            .collection("requests")
+            .updateOne(
+              { _id: existing._id },
+              { $set: { number: existing.number } }
+            );
+
+          const updateUserRes = await db
+            .collection("users")
+            .updateOne(
+              { membership_id, "requested_components.name": component_name },
+              { $inc: { "requested_components.$.number": number } }
+            );
+
+          return new Response(
             JSON.stringify({
-              message: "component not found",
+              message: "request made successfully",
             }),
             {
-              status: 350,
+              status: 200,
               headers: {
                 "Content-Type": "application/json",
               },
             }
-          ); 
+          );
+        } else {
+          return new Response(
+            JSON.stringify({
+              message: "item's available number is less than the request",
+            }),
+            {
+              status: 300,
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        }
+      }
+      const newRequest: Request = {
+        component_name: component_name,
+        membership_id: membership_id,
+        number: number,
+      };
+      const newRequestres = await db
+        .collection("requests")
+        .insertOne(newRequest);
+      const requested_component = {
+        name: component_name,
+        number: number,
+      };
+      const updateUserRes = await db
+        .collection("users")
+        .updateOne(
+          { membership_id: membership_id },
+          { $push: { requested_components: requested_component } }
+        );
+      return new Response(
+        JSON.stringify({
+          message: "request made successfully",
+        }),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
     }
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: "component not found",
+      }),
+      {
+        status: 350,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
